49223079a8b9f77f5cc66ee446ff369e210d5830
Jimmy Blundell <jblundell123@gmail.com>
2020-03-26 16:09:44 -0500

    Added new icons, switched up home screen menu. Created new activity for routines and exercises pages.

--- app/src/main/java/com/example/gainzjournal/MainActivity.java
+++ app/src/main/java/com/example/gainzjournal/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.gainzjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button routines_button;
+    private Button exercises_button;
+    private Button logs_button;
+    private Button records_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
